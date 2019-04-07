d2e59543f3697fc6fec190170aca97aca1000c2b
iyotti <neuroooo100@gmail.com>
2019-04-07 23:16:49 +0900

    add:set_prefix.py

---
+++ set_prefix.py
@@ -0,0 +1,10 @@
+import glob
+import os
+
+#path = "./prefixtest"
+path = input("prefixを付けたいディレクトリを入力：")
+pfx  = input("prefixを入力：")
+files = glob.glob(path+"/*")
+
+for f in files:
+	os.rename(f,os.path.join(path,pfx+os.path.basename(f)))
